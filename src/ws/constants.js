db5e431f746a69eb718df38c2367ee931514d379
Tarsis <br.tmvdl@gmail.com>
2024-01-14 19:00:39 -0300

    Updating ws files

---
+++ src/ws/constants.js
@@ -0,0 +1,25 @@
+
+export const CLOCK = {
+  HOURS: 60 * 60,
+  MINUTES: 60,
+  SECONDS: 1,
+}
+
+export const PAIRS = [
+  ['BTC', 'BRL'],
+  ['USDT', 'BRL'],
+  ['ETH', 'BRL'],
+  ['XRP', 'BRL'],
+  ['BNB', 'BRL'],
+  ['MATIC', 'BRL'],
+  ['SOL', 'BRL'],
+  ['LINK', 'BRL'],
+  ['LTC', 'BRL'],
+  ['AVAX', 'BRL'],
+  ['DOGE', 'BRL'],
+  ['ADA', 'BRL'],
+  ['DOT', 'BRL'],
+  ['BUSD', 'BRL'],
+  ['CHZ', 'BRL'],
+  //
+]
--- src/ws/index.js
+++ src/ws/index.js
@@ -1,5 +1,13 @@
 import http from 'http'
 import { Server } from 'socket.io'
+import * as constants from './constants.js'
+
+import { Database } from '@brtmvdl/database'
+const db = new Database({ config: '/data', type: 'fs' })
+
+const savePairsPrices = (prices = [], datetime = Date.now()) => prices.map(({ symbol, price } = {}) => {
+  db.in('prices').new().writeMany({ symbol, price, datetime })
+})
 
 const server = http.createServer()
 const io = new Server(server, { cors: { origin: '*' } })
@@ -10,6 +18,14 @@ io.on('connection', socket => {
   socket.on('event', data => console.log('event', data))
 
   socket.on('hello', () => console.log('from client'))
+
+  socket.on('list pairs', () =>
+    fetch(`https://api4.binance.com/api/v3/ticker/price?symbols=[${constants.PAIRS.map(([a, b]) => `"${a}${b}"`).join(',')}]`)
+      .then((res) => res.json())
+      .then((prices) => [socket.emit('list pairs', prices), savePairsPrices(prices)])
+      .then(() => console.log('list pairs'))
+      .catch((err) => console.error(err))
+  )
 
   socket.on('disconnect', () => console.log('disconnect', socket))
 })
