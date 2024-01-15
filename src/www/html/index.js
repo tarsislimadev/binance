975a7f77f409dcc804f8fbc07b38cc6ac0f7cb98
Tarsis <br.tmvdl@gmail.com>
2024-01-14 21:40:15 -0300

    Updating www files

--- src/www/html/index.js
+++ src/www/html/index.js
@@ -1,4 +1,4 @@
-import { HTML, nButton } from '@brtmvdl/frontend'
+import { HTML, nButton, nFlex } from '@brtmvdl/frontend'
 import { io } from 'socket.io-client'
 
 export class Page extends HTML {
@@ -8,28 +8,69 @@ export class Page extends HTML {
   }
 
   children = {
+    pairs_list: new HTML(),
     list_pairs_button: new nButton(),
     check_server_time: new nButton(),
-    pairs: new HTML(),
   }
 
   onCreate() {
     this.setText('Binance')
+    this.setMessages()
     this.setSocketMessages()
     this.append(this.getListPairsButton())
     this.append(this.getCheckServerTimeButton())
-    this.append(this.getPairs())
+    this.append(this.getPairsList())
+  }
+
+  setMessages() {
+    this.on('buy', (data) => this.onBuyButtonClick(data))
   }
 
   setSocketMessages() {
-    this.state.socket.on('connect', () => this.state.socket.emit('hello'))
+    this.state.socket.on('connect', () => this.onConnect())
     this.state.socket.on('hello', () => console.log('server hello'))
 
-    this.state.socket.on('symbol price ticker', (data) => console.log('symbol price ticker', data))
+    this.state.socket.on('symbol price ticker', (data) => this.onSymbolPriceTicker(data))
     this.state.socket.on('error symbol price ticker', (err) => console.error(err))
 
-    this.state.socket.on('check server time', (data) => console.log('check server time', data))
+    this.state.socket.on('check server time', (data) => this.onCheckServerTime(data))
     this.state.socket.on('error check server time', (err) => console.error(err))
+  }
+
+  onConnect() {
+    this.state.socket.emit('hello')
+  }
+
+  onSymbolPriceTicker(pairs) {
+    const pairs_list = new HTML()
+
+    pairs.map(({ symbol, price }) => {
+      const flex = new nFlex()
+      flex.setStyle('', '')
+      flex.append((new HTML()).setText(symbol))
+      flex.append((new HTML()).setText(price))
+      flex.append(this.getBuyButton(symbol))
+      pairs_list.append(flex)
+    })
+
+    this.children.pairs_list.clear()
+    this.children.pairs_list.append(pairs_list)
+  }
+
+  onCheckServerTime(data) {
+    console.log('check server time', data)
+  }
+
+  onBuyButtonClick({ value } = {}) {
+    this.state.socket.emit('buy', value)
+  }
+
+  getBuyButton(symbol, amount = 1) {
+    const button = new nButton()
+    const text = `buy ${amount}${symbol}`
+    button.setText(text)
+    button.on('click', () => this.dispatchEvent('buy', { symbol, amount }))
+    return button
   }
 
   getListPairsButton() {
@@ -44,7 +85,7 @@ export class Page extends HTML {
     return this.children.check_server_time
   }
 
-  getPairs() {
-    return this.children.pairs
+  getPairsList() {
+    return this.children.pairs_list
   }
 }
