a89e47740dd2c944f502d23fc028243a4e4224b2
Tarsis <br.tmvdl@gmail.com>
2024-01-15 16:14:09 -0300

    Updating ws files

--- src/ws/index.js
+++ src/ws/index.js
@@ -13,24 +13,24 @@ io.on('connection', socket => {
 
   socket.on('hello', () => console.log('client hello'))
 
-  const getPriceTicker = () => api.tickerPrice(constants.PAIRS)
+  const priceTickers = () => api.tickerPrice(constants.PAIRS)
     .then((prices) => [socket.emit('symbol price ticker', prices), database.savePairsPrices(prices)])
-    .then(() => getPriceTicker())
     .catch((err) => socket.emit('error symbol price ticker', err))
+    .finally(() => priceTickers())
 
-  getPriceTicker()
+  priceTickers()
 
-  const getServerTime = () => api.time()
+  const onServerTime = () => api.time()
     .then((time) => socket.emit('check server time', time))
     .catch((err) => socket.emit('error check server time', err))
 
-  socket.on('check server time', () => getServerTime())
+  socket.on('check server time', () => onServerTime())
 
-  const buy = (symbol, price, amount = 1, datetime = Date.now()) => database.saveBuy(symbol, price, amount, datetime)
+  const onBuy = (symbol, price, amount = 1, datetime = Date.now()) => database.saveBuy(symbol, price, amount, datetime)
     .then((buy) => socket.emit('buy', { symbol, price, amount, datetime }))
     .catch((err) => socket.emit('buy error', err))
 
-  socket.on('buy', ({ symbol, price, amount = 1 } = {}) => buy(symbol, price, amount))
+  socket.on('buy', ({ symbol, price, amount = 1 } = {}) => onBuy(symbol, price, amount))
 
 })
 
